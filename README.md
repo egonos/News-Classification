72cd1c5ad2f5695e651fab70cea65d891340b3d9
Egemen <egedalgic@gmail.com>
2025-05-26 14:18:48 +0300

    project refarrangement

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Project Overview
-This project deploys a Large Language Model (LLM) for technology news classification. News articles are collected daily via Apache Airflow, classified using an LLM, and the results are stored in a PostgreSQL database. Model tracking is handled via MLflow.
+This project deploys a Large Language Model (LLM) for news classification. News articles are collected weekly via Apache Airflow, classified using an LLM, and the results are stored in a PostgreSQL database. Model tracking is handled via MLflow.
 
 ## Model and Hyperparameter Selection
-Three different text classification models, each with varying maximum token lengths, were evaluated using the IMDB, Amazon, and mixed reviews datasets. Based on classification performance (Accuracy and ROC-AUC scores) and average response time, the most efficient model was selected: "siebert/sentiment-roberta-large-english" (`max_length = 128`).
+Three different text classification models, each with varying maximum token lengths, were evaluated using the IMDB, Amazon, and mixed reviews datasets. Based on classification performance (Accuracy and ROC-AUC scores) and average response time, the most efficient model was selected: `siebert/sentiment-roberta-large-english (max_length = 128)`.
 
 
 <img src="images/model_performances.png" alt="Model Performances" width="800">
@@ -12,10 +12,17 @@ Also detailed experiment results are available in `model_selection/model_logs.cs
 ## Project Architecture
 Airflow DAG --> LLM Model Inference --> Postgres Logging --> MLflow Tracking
 
-1. Airflow is used for daily news collection. You can trigger the dag manually as well.
-2. The model classifies the news as it receives it. The labels are positive (1) and negative (0).
+1. Airflow is used for weekly news collection. You can trigger the dag manually as well.
+2. The model classifies the news as it receives it. The labels are POSITIVE and NEGATIVE.
 3. The news, the topic and the classification results are logged in a Postgres database.
-4. During the process, the model specs like response time, device it used, the label tracked in a MLFlow Server.
+
+<img src="images/postgres_table.png" alt="Database" width="800">
+
+4. During the process, model specifications such as the device used and the predicted label's confidence score are tracked on the MLflow server. These MLflow records can be matched with the corresponding entries in the PostgreSQL database by using the unique IDs assigned to each instance.
+
+<img src="images/mlflow_table1.png" alt="MLFlow1" width="800">
+
+<img src="images/mlflow_table2.png" alt="MLFlow2" width="800">
 
 ## Setup Instructions
 
@@ -34,21 +41,21 @@ docker compose --compatibility up --build -d
 
 Interface links:
 
-* Airflow UI: `http://localhost:8080` (User name: egemen, Password: egemen)
+* Airflow UI: `http://localhost:8080` (**User name:** egemen, **Password:** egemen)
 * MLFlow UI: `http://localhost:5000`
 
 Configure and Trigger Dag:
 
 * Dag name: `news_classification_dag`
-* Triggered daily but it could be also triggered manually.
+* Triggered weekly but it could be also triggered manually.
 
 To view the results table (after running the algorithm), you can download and use DBeaver:
 
 * Click Database
 * Select `New Database Connection`
 * Select `postgres`
-* Database: `airflow`, username: `airflow`, password: `airflow`
+* **Database:** `airflow`, **User name:** `airflow`, **Password:** `airflow`
 
 
 ## Further Improvements
-The models were evaluated using review datasets. More relevant data could be used for fine-tuning the LLM model, which would improve its performance.
+The models were evaluated using review datasets. More relevant data could be used for fine-tuning the LLM model, which would improve its performance. Also since the news are collected weekly, the latency of the model may not be as important; therefore a bigger model with slightly higher performance could be selected. Lastly, since the free version of the API is used, only ten news articles per topic can be fetched. For a more comprehensive and realistic analysis, as many news articles as possible can be retrieved using the paid version.
--- dags/news_sentiment_dag.py
+++ dags/news_sentiment_dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import mlflow
 import time
@@ -12,46 +12,58 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipe
 import torch
 
 API_KEY = "c1607c63d3ae457898ed5ad03ddb36bb"
-QUERY = "technology"
-PAGE_SIZE = 3
+QUERY_LIST = ["technology", "finance", "health"]
+PAGE_SIZE = 10
 MODEL = "siebert/sentiment-roberta-large-english"
 MAX_LENGTH = 128
 MLFLOW_TRACKING_URI = "http://mlflow_server:5000"
 
+def pull_news(**kwargs):
+    all_news_items = []
+    timeline = (datetime.today()- timedelta(days=7)).strftime('%Y-%m-%d') 
 
-def pull_news(query=QUERY, page_size=PAGE_SIZE, **kwargs):
+    #pull three news for each topic
+    for query in QUERY_LIST:
+        url = (
+            f"https://newsapi.org/v2/everything?"
+            f"q={query}&"
+            f"pageSize={PAGE_SIZE}&"
+            f"language=en&"
+            f"from={timeline}&"
+            f"sortBy=publishedAt&"
+            f"apiKey={API_KEY}"
+        )
 
-    url = f"https://newsapi.org/v2/everything?q={query}&pageSize={page_size}&language=en&sortBy=publishedAt&apiKey={API_KEY}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise Exception(f"Error fetching news for {query}: {e}")
 
-    # fetch the data and check the presence of an error
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        raise Exception(f"Error fetching news: {e}")
+        articles = response.json().get("articles", [])
+        #separate the details of each news and store them in a dictionary
+        texts = [
+            {
+                "news_id": str(uuid.uuid4()),
+                "text": f"{a['title']} {a.get('description', '')}".strip(),
+                "topic": query
+            }
+            for a in articles
+        ]
 
-    # get the data
-    articles = response.json().get("articles", [])
-    texts = [
-        {
-            "news_id": str(uuid.uuid4()),
-            "text": f"{a['title']} {a.get('description', '')}".strip(),
-        }
-        for a in articles
-    ]
+        #combine all the dictionaries in a list
+        all_news_items.extend(texts)
 
-    # push the data using xcom
-    kwargs["ti"].xcom_push(key="news_items", value=texts)
-
+    #push the results to xcom
+    kwargs["ti"].xcom_push(key="news_items", value=all_news_items)
 
 def run_sentiment_pipeline(**kwargs):
 
-    # pull the news texts using xcom
+    #pull the news using xcom and separate ids and texts
     news_items = kwargs["ti"].xcom_pull(key="news_items", task_ids="pull_news_task")
     ids = [text["news_id"] for text in news_items]
     texts = [text["text"] for text in news_items]
 
-    # build the classification pipeline
     tokenizer = AutoTokenizer.from_pretrained(MODEL)
     model = AutoModelForSequenceClassification.from_pretrained(MODEL)
     device = 0 if torch.cuda.is_available() else -1
@@ -64,88 +76,72 @@ def run_sentiment_pipeline(**kwargs):
         truncation=True,
     )
 
-    # run the classification pipeline and log the results in MLflow
     start_time = time.time()
     sentiment_results = sentiment_pipeline(texts)
     end_time = time.time()
 
-    # track the sentiment results and news ids in MLflow
+    #track the results with mlflow
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     with mlflow.start_run():
-        for idx, sentiment_result in zip(ids, sentiment_results):
+        for item, sentiment_result in zip(news_items, sentiment_results):
             label = sentiment_result["label"]
             score = sentiment_result["score"]
-            confidence_score = 1 - score if label == "NEGATIVE" else score
+            
 
-            mlflow.log_metric(
-                f"News ID {idx} -  Positive Probability", confidence_score
-            )
-            mlflow.log_param(f"News ID {idx} - Predicted Label", label)
+            mlflow.log_metric(f"{item['topic']} - {item['news_id']} - Confidence", score)
+            mlflow.log_param(f"{item['topic']} - {item['news_id']} - Label", label)
 
         mlflow.log_param("Model Name", MODEL)
         mlflow.log_param("Max Token Length", MAX_LENGTH)
         mlflow.log_param("Device", "GPU" if torch.cuda.is_available() else "CPU")
-        mlflow.log_metric(
-            "Average Inference Time", (end_time - start_time) / len(texts)
-        )
+        
 
-    # push the sentiment results using xcom to store them in a Postgres table
     kwargs["ti"].xcom_push(key="sentiment_results", value=sentiment_results)
     kwargs["ti"].xcom_push(key="news_ids", value=ids)
 
-
 def save_results_to_postgres(**kwargs):
 
-    # pull the news texts and sentiment results
+    #pull the results as well as news using xcom 
     sentiment_results = kwargs["ti"].xcom_pull(
         key="sentiment_results", task_ids="run_sentiment_pipeline_task"
     )
     news_items = kwargs["ti"].xcom_pull(key="news_items", task_ids="pull_news_task")
-    ids = [item["news_id"] for item in news_items]
-    texts = [item["text"] for item in news_items]
 
-    # connect to postgres
     hook = PostgresHook(postgres_conn_id="news_records")
     conn = hook.get_conn()
     cursor = conn.cursor()
 
-    # create a table if it doesn't exist
+    #create the table
     cursor.execute(
         """
-                    CREATE TABLE IF NOT EXISTS news_sentiment_results (
-                        id SERIAL PRIMARY KEY,
-                        news_id TEXT,
-                        topic TEXT,
-                        news_text TEXT,
-                        predicted_label TEXT,
-                        prediction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                    );
-                   """
+        CREATE TABLE IF NOT EXISTS news_sentiment_results (
+            id SERIAL PRIMARY KEY,
+            news_id TEXT,
+            topic TEXT,
+            news_text TEXT,
+            predicted_label TEXT,
+            prediction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """
     )
 
-    # insert news texts and sentiment analysis results to the PostgreSQL table
-    for idx, sentiment_result, news_text in zip(ids, sentiment_results, texts):
-        predicted_label = sentiment_result["label"]
-
+    for item, sentiment_result in zip(news_items, sentiment_results):
         cursor.execute(
             """
             INSERT INTO news_sentiment_results (news_id, topic, news_text, predicted_label)
-            VALUES ( %s, %s, %s, %s);
+            VALUES (%s, %s, %s, %s);
             """,
-            (idx, QUERY, news_text, predicted_label),
+            (item["news_id"], item["topic"], item["text"], sentiment_result["label"]),
         )
 
-    # commit the changes and close the cursor and connection
     conn.commit()
     cursor.close()
     conn.close()
 
-
-# define the DAG
 with DAG(
     dag_id="news_sentiment_dag",
     start_date=datetime(2024, 1, 1),
-    schedule="@daily",
+    schedule="@weekly",
     catchup=False,
 ) as dag:
 
